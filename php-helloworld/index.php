ee796b967817d8e082a1a8f150b01afcdc6dc6d7
Student User <student@workstation.lab.example.com>
2022-11-23 04:21:42 -0500

    Changed index page contents2.

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,5 +1,5 @@
 <?php
 print "Hello, World! php version is::: " . PHP_VERSION . "\n";
-print "NEW Line : TEST 2022 \n";
+print "NEW online ---SAM---- : TEST 2022 \n";
 Print "A change is a coming!\n";
 ?>
